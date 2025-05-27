598e4f14cad86609e143e5943c57adc0960f13c5
EliethU <izaduartes2214@gmail.com>
2025-05-26 22:39:15 -0600

    se pudo quitar el error del App.css

---
+++ src/assets/database/authcontext.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useContext, useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { appfirebase } from "../database/firebaseconfig";
+
+// Crear el contexto
+const AuthContext = createContext();
+
+// Hook personalizado para consumir el contexto
+export const useAuth = () => useContext(AuthContext);
+
+// Proveedor de contexto
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isOffline, setIsOffline] = useState(!navigator.onLine);
+
+
+  // Verificar si el usuario está autenticado
+  useEffect(() => {
+    const auth = getAuth(appfirebase);
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setIsLoggedIn(!!user);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  // Cerrar sesión
+  const logout = async () => {
+    const auth = getAuth(appfirebase);
+    await signOut(auth);
+    setIsLoggedIn(false);
+  };
+
+  // Valores disponibles para los componentes hijos
+  return (
+    <AuthContext.Provider value={{ user, isLoggedIn, isOffline, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/assets/database/firebaseconfig.jsx
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnVpSmOJlZM5w0qEjUs7vOhfpoRu2hu5o",
+  authDomain: "ferreteriaduarte-afcca.firebaseapp.com",
+  projectId: "ferreteriaduarte-afcca",
+  storageBucket: "ferreteriaduarte-afcca.firebasestorage.app",
+  messagingSenderId: "857829337329",
+  appId: "1:857829337329:web:d0eb4f40a710d1f8412150",
+  measurementId: "G-G21VYYZZM7"
+};
+
+// Initialize Firebase
+const appfirebase = initializeApp(firebaseConfig);
+const db = getFirestore(appfirebase);
+const auth = getAuth(appfirebase);
+const analytics = getAnalytics(app);
+
+export { appfirebase, db, auth, analytics };
